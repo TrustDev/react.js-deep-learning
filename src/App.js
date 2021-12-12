08e3fe9a164aba25975f633a19afa143b7522bc3
React/ReactNative developer <67072920+TrustDev@users.noreply.github.com>
2021-12-12 22:37:59 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
 import "./styles.css";
-import React from 'react';
-import {ThemeContext,themes} from './theme-context';
-import ThemedButton from './themed-button';
+import React from "react";
+import { ThemeContext, themes } from "./theme-context";
+import ThemedButton from "./themed-button";
+
+const UserContext = React.createContext({
+  name: "Guest"
+});
 
 function Toolbar(props) {
+  return <ThemedButton onClick={props.changeTheme}>Change Theme</ThemedButton>;
+}
+
+function Layout() {
   return (
-    <ThemedButton onClick={props.changeTheme}>
-      Change Theme
-    </ThemedButton>
-  )
+    <div>
+      <Content />
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <UserContext.Consumer>
+          {(user) => <ProfilePage user={user} theme={theme} />}
+        </UserContext.Consumer>
+      )}
+    </ThemeContext.Consumer>
+  );
+}
+
+function ProfilePage() {
+  return <div></div>;
 }
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.toggleTheme = () => {
-      console.log(this.state.theme)
-      this.setState(state => ({
-        theme:
-          state.theme === themes.dark
-            ? themes.light : themes.dark
+      console.log(this.state.theme);
+      this.setState((state) => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark
       }));
     };
     this.state = {
       theme: themes.light,
       toggleTheme: this.toggleTheme
-    }
+    };
   }
 
   render() {
+    const { signedInUser } = this.props;
     return (
-        <ThemeContext.Provider value={this.state.theme}>
+      <ThemeContext.Provider value={this.state.theme}>
+        <UserContext.Provider value={signedInUser}>
           <Toolbar changeTheme={this.toggleTheme} />
-        </ThemeContext.Provider>
-    )
+          <Layout />
+        </UserContext.Provider>
+      </ThemeContext.Provider>
+    );
   }
 }
