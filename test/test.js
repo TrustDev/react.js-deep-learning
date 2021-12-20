73349d740f77a914a8afaf9602ca7ce086d3b83f
TrustDev <67072920+TrustDev@users.noreply.github.com>
2021-12-20 09:05:46 +0300

    test mocha mistake

--- test/test.js
+++ test/test.js
@@ -8,6 +8,12 @@ describe('Array', function() {
       setImmediate(done);
       setImmediate(done);
     })
+    it('should complete this test', function(done) {
+      return new Promise(function (resolve) {
+        assert.ok(true);
+        resolve();
+      }).then(done);
+    })
   })
 })
 describe('User', function() {
