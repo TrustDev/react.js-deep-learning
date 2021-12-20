4e389e956d784553a1bf6fe6f350ef715099cb45
TrustDev <67072920+TrustDev@users.noreply.github.com>
2021-12-20 09:02:03 +0300

    throw error in done

--- test/test.js
+++ test/test.js
@@ -10,3 +10,10 @@ describe('Array', function() {
     })
   })
 })
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      done('error');
+    })
+  })
+})
