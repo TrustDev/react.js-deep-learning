6780f497f10be5442a0cc478417a066b84104e8d
TrustDev <67072920+TrustDev@users.noreply.github.com>
2021-12-20 09:14:27 +0300

    fix synchronize test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), -1);      
     })
     it('double done', function(done) {
       setImmediate(done);
@@ -13,6 +13,9 @@ describe('Array', function() {
         assert.ok(true);
         resolve();
       }).then(done);
+    })
+    it('should return -1 when the value is not present', function() {
+      [1, 2, 3].indexOf(5).should.equal(-1);      
     })
   })
 })
